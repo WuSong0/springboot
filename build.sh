585a08fa11fb4326e4bc302b22feb8177c4fbb25
WuSong <18371118883@163.com>
2018-03-09 17:19:30 +0800

    添加权限

--- build.sh
+++ build.sh
@@ -2,10 +2,12 @@
 
 IMAGE=springboot
 
-docker stop myapp
+USER root
+
+echo "visenergy" | sudo -S docker stop myapp
 
 #mvn package -e -X docker:build -DskipTest
 
 sleep 3s
 
-docker run -d -p 9090:8080 --name myapp  ${IMAGE}
+echo "visenergy" | sudo -S docker run -d -p 9090:8080 --name myapp  ${IMAGE}
